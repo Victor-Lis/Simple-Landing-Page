d8cd597a86b8befb2091ad70c8bb08ffbaefdb19
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2022-12-16 10:56:15 -0300

    v1

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,16 @@ const myAge = document.getElementById("my-years-old")
 
 var today = new Date();
 var year = today.getFullYear();
+var month = today.getMonth()
+var day = today.getDate()
 
 function setAge(){
+
+    if(month < 2 || month == 2 && day < 16){
+
+        year--
+
+    }
 
     myAge.innerText = `I'm ${year-2007} years old and I'm studding programming, this website is a someone of my projects, I intend to work more on the front end.`
 
