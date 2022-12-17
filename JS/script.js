e0e4f02dcc073f9b83514a3091682708b25db027
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2022-12-17 16:34:33 -0300

    v1

--- JS/script.js
+++ JS/script.js
@@ -13,7 +13,7 @@ function setAge(){
 
     }
 
-    myAge.innerText = `I'm ${year-2007} years old and I'm studding programming, this website is a someone of my projects, I intend to work more on the front end.`
+    myAge.innerText = `I'm ${year-2007} years old and I'm studyng programming, this website is a someone of my projects, I intend to work more on the front end.`
 
 }
 
